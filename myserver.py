311db8c719da0f43544ce7e3108f0d482288c377
EXE X <scra6416@gmail.com>
2025-01-16 14:38:30 +0700

    Update myserver.py

--- myserver.py
+++ myserver.py
@@ -8,15 +8,12 @@ app = Flask('')
 def home():
     return "Server is running!"
 
-# ฟังก์ชันที่ใช้สำหรับรัน Flask app
 def run():
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
 
-# ฟังก์ชันเพื่อเริ่มต้น Thread ที่รัน Flask app
 def server_on():
     t = Thread(target=run)
     t.start()
 
-# เรียกใช้ฟังก์ชัน server_on() เมื่อบอททำงาน
 if __name__ == "__main__":
     server_on()
